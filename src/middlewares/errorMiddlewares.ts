fa15720dc8ba6ac50075fc926602f8a8400a5d63
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-02-25 14:26:17 +0100

    Add errorMiddlewares

---
+++ src/middlewares/errorMiddlewares.ts
@@ -0,0 +1,28 @@
+import createDebug from "debug";
+import { type Response, type Request, type NextFunction } from "express";
+import { CustomError } from "../CustomError/CustomError";
+
+const debug = createDebug("users:server");
+
+export const notFoundError = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const error = new CustomError("Path not found", 404, "Endpoint not found");
+
+  next(error);
+};
+
+export const generalError = (
+  error: CustomError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  debug(error.message);
+
+  res
+    .status(error.statusCode || 500)
+    .json({ error: error.publicMessage || "Something went wrong." });
+};
