d108ebaff40530b646a4d4dff79571f6911d8c6c
victorandujar <victorandujarmurcia@gmail.com>
2023-02-25 16:53:04 +0100

    Correct missing await at registerUser

---
+++ src/server/controllers/usersControllers.ts
@@ -0,0 +1,60 @@
+import { type NextFunction, type Request, type Response } from "express";
+import { CustomError } from "../../CustomError/CustomError.js";
+import { User } from "../../database/models/Users.js";
+import { type UserRegister } from "../../types/types.js";
+import bcryptjs from "bcryptjs";
+
+export const getUsers = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const users = await User.find().exec();
+
+    res.status(200).json({ users });
+  } catch (error) {
+    const customError = new CustomError(
+      (error as Error).message,
+      500,
+      "Couldn't retrieve users."
+    );
+
+    next(customError);
+  }
+};
+
+export const registerUser = async (
+  req: Request<Record<string, unknown>, Record<string, unknown>, UserRegister>,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { email, name, password, phoneNumber, username, enemies, friends } =
+      req.body;
+    const image = req.file?.filename;
+
+    const hashedPassword = await bcryptjs.hash(password, 10);
+
+    const user = await User.create({
+      email,
+      name,
+      password: hashedPassword,
+      phoneNumber,
+      username,
+      image,
+      enemies,
+      friends,
+    });
+
+    res.status(201).json({ user });
+  } catch (error) {
+    const customError = new CustomError(
+      "Couldn't create the user.",
+      500,
+      "Couldn't create the user."
+    );
+
+    next(customError);
+  }
+};
